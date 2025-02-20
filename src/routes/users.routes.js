89ac27ac93adc37df9b2e40e37d74a5d90fc0a5c
jhil <jhilbhavsar0@gmail.com>
2025-02-20 18:07:10 +0530

    login API done

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser,loginUser } from "../controllers/userController.js";
 
 const router = Router()
 
 router.route("/register").post(createUser)
+router.route("/login").post(loginUser)
 export default router   
