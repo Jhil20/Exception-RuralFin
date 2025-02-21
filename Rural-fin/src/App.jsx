a0242f8e63421360801d9918ee501ca88336adad
harshp4114 <harshpatadia4114@gmail.com>
2025-02-22 01:05:23 +0530

    setup ready with vite and tailwind

---
+++ Rural-fin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-serif text-center'>hello this is harsh</h1>
+    </>
+  )
+}
+
+export default App
