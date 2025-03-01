293e4c5a36f47da4ac830f1b00b16fd097b3cc1a
jhil <jhilbhavsar0@gmail.com>
2025-03-01 11:28:18 +0530

    deposit API

--- server/src/routes/agent.routes.js
+++ server/src/routes/agent.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {createAgent, loginAgent, logoutAgent,walletCreation,securityDepositPayment,verifyPayment}from "../controllers/agentController.js"
+import { userAgentTransactionDeposit } from "../controllers/transactionController.js";
 
 const routerAgent= Router()
 routerAgent.route("/register").post(createAgent)
@@ -9,5 +10,6 @@ routerAgent.route("/logout").post(verifyJWT,logoutAgent)
 routerAgent.route("/walletCreation").post(walletCreation)
 routerAgent.route("/securityPayment").post(securityDepositPayment)
 routerAgent.route("/verifyPayment").post(verifyPayment)
+routerAgent.route("/userDeposit").post(userAgentTransactionDeposit)
 export default routerAgent;
 
