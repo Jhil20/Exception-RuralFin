15497749f4cea19f0a56e53bd2eaa022eed12c8b
harshp4114 <harshpatadia4114@gmail.com>
2025-06-21 15:38:55 +0530

    commit for deploy after speak added

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL="http://localhost:5000";
-// export const BACKEND_URL="https://exception-ruralfin.onrender.com";
+// export const BACKEND_URL="http://localhost:5000";
+export const BACKEND_URL="https://exception-ruralfin.onrender.com";
 
