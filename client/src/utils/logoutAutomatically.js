2f0cc7085ead71b8fd363467e36af5db52c04b5d
harshp4114 <harshpatadia4114@gmail.com>
2025-06-24 20:13:09 +0530

    socket for budget update in expense anlaytics done and commit for deploy 2

--- client/src/utils/logoutAutomatically.js
+++ client/src/utils/logoutAutomatically.js
@@ -7,7 +7,7 @@ import { NotSignedIn } from "../redux/slices/isSignInSlice";
 
 const logoutAutomatically = () => {
   const token = Cookies.get("token");
-  const dispatch = useDispatch();
+//   const dispatch = useDispatch();
   const location = useLocation();
 
   if (token) {
@@ -18,8 +18,8 @@ const logoutAutomatically = () => {
     ) {
       const decoded = jwtDecode(token);
       disconnectSocket();
-      dispatch(NotSignedIn());
-      Cookies.remove("token");
+    //   dispatch(NotSignedIn());
+    //   Cookies.remove("token");
     }
   }else{
     console.log("No token found, user is not signed in.");
